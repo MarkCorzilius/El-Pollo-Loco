e50f51318786b565673683bf7384d1c809dd5337
Mark Corzilius <marccorzprivate@gmail.com>
2025-06-21 16:17:46 +0200

    created endboss, animated alerted state of endboss

---
+++ models/endboss.class.js
@@ -0,0 +1,30 @@
+class Endboss extends movableObject {
+    height = 220;
+    width = 220;
+    x = 300;
+    y = 220;
+    IMAGES_WALKING = [
+        './img/4_enemie_boss_chicken/2_alert/G5.png',
+        './img/4_enemie_boss_chicken/2_alert/G6.png',
+        './img/4_enemie_boss_chicken/2_alert/G7.png',
+        './img/4_enemie_boss_chicken/2_alert/G8.png',
+        './img/4_enemie_boss_chicken/2_alert/G9.png',
+        './img/4_enemie_boss_chicken/2_alert/G10.png',
+        './img/4_enemie_boss_chicken/2_alert/G11.png',
+        './img/4_enemie_boss_chicken/2_alert/G12.png',
+    ]
+
+    constructor(){
+        super();
+        this.loadImage(this.IMAGES_WALKING[0]);
+        this.loadMovementSprites(this.IMAGES_WALKING);
+        this.playObjectAnimation(this.IMAGES_WALKING);
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            this.playObjectAnimation(this.IMAGES_WALKING)
+        }, 1000);
+    }
+}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,34 +1,41 @@
 class movableObject {
-    x = 120;
-    y = 325;
-    img;
-    width = 100;
-    height = 100;
-    imageCache = {};
-    currentImage = 0;
-    speed = 0.15;
-    otherDirection = false;
+  x = 120;
+  y = 325;
+  img;
+  width = 100;
+  height = 100;
+  imageCache = {};
+  currentImage = 0;
+  speed = 0.15;
+  otherDirection = false;
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
-    loadMovementSprites(arr){
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+  loadMovementSprites(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 
-    moveRight() {
-        console.log('Go right!');
-    }
+  moveRight() {
+    console.log("Go right!");
+  }
 
-    moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000/60);
-    }
+  moveLeft() {
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
+  }
+
+  playObjectAnimation(images) {
+    let i = this.currentImage % images.length;
+    let path = images[i];
+    this.img = this.imageCache[path];  
+    this.currentImage++; 
+}
 }
