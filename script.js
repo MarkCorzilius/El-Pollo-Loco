670e1f4b25b14d89ff6feb6be1c8df65c63339a2
Mark Corzilius <marccorzprivate@gmail.com>
2025-07-03 20:09:57 +0200

    full screen mode and all buttons styling adjusted

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let globalEnemyId = 0;
+
+function enterFullScreen() {
+    const canvas = document.getElementById('canvas');
+    canvas.requestFullscreen();
+}
+
+function toggleGameVolume() {
+    const button = document.getElementById('volumeBtn');
+
+    // Check if current image is volume-on
+    if (button.src.includes("volume-on.png")) {
+        button.src = "./img/volume-off.png";
+    } else {
+        button.src = "./img/volume-on.png";
+    }
+}
